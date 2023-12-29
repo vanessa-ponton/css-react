ac045b6dcbcfd090c59c5cc8006742ad7bb2fcaa
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-12-29 12:00:49 +0100

    react-css

---
+++ css-react/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import './style.scss';
+import image from "./assets/2168.jpg";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Bio</li>
+          <li>Programme</li>
+          <li>Vidéo</li>
+        </ul>
+      </nav>
+    <h1>NessaGaming</h1>
+    <img style={{ width: 100, height: 100 }}className="Image" src= {image} alt="image"/>
+    </div>
+  )
+}
+
+export default App;
