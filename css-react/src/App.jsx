94d95a2f506554cce10fd2b952f12de540565e10
Vanessa Ponton <vanessa.ponton@gmail.com>
2024-01-03 11:31:35 +0100

    css-react

--- css-react/src/App.jsx
+++ css-react/src/App.jsx
@@ -1,22 +1,34 @@
 import "./App.css";
-import './style.scss';
+import "./style.scss";
 import image from "./assets/2168.jpg";
+import style from "styled-components";
 
 function App() {
+  const Card = style.div`
+  background: rgb(77, 206, 245);
+  display: block;
+  text-align: center;
+  `;
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>Bio</li>
-          <li>Programme</li>
-          <li>Vidéo</li>
-        </ul>
-      </nav>
-    <h1>NessaGaming</h1>
-    <img style={{ width: 100, height: 100 }}className="Image" src= {image} alt="image"/>
-    </div>
-  )
+    <Card>
+      <div>
+        <h1>NessaGaming</h1>
+        <img
+          style={{ width: 100, height: 100 }}
+          className="Image"
+          src={image}
+          alt="image"
+        />
+        <nav>
+          <ul>
+            <li>Bio</li>
+            <li>Programme</li>
+            <li>Vidéos</li>
+          </ul>
+        </nav>
+      </div>
+    </Card>
+  );
 }
 
 export default App;
-git
